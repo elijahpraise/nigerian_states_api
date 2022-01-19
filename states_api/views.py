f1f85901e42677b364589dd925af34cc3de7265f
elijahpraise <elijahpraise21@gmail.com>
2022-01-19 23:52:31 +0200

    second commit

--- states_api/views.py
+++ states_api/views.py
@@ -5,14 +5,24 @@ from states_api.serializers import *
 from states_api.models import *
 
 
+# class NigerianStateView(generics.CreateAPIView):
+#
+#     def post(self, request, *args, **kwargs):
+#         name_of_state = request.data.get("name_of_state").capitalize()
+#         for states in NigerianState.objects.all():
+#             if states.name_of_state == name_of_state:
+#                 governor_of_that_state = Governor.objects.get(state_id=states.pk)
+#                 name = GovernorSerializer(governor_of_that_state).data.get('name', None)
+#                 deputy = DeputyGovernorSerializer(DeputyGovernor.objects.get(state_id=states.pk)).data.get('name',
+#                                                                                                            None)
+#                 return Response({"Governor": name, "Deputy Governor": deputy})
+
+
 class NigerianStateView(generics.CreateAPIView):
 
     def post(self, request, *args, **kwargs):
-        name_of_state = request.data.get("name_of_state").capitalize()
-        for states in NigerianState.objects.all():
-            if states.name_of_state == name_of_state:
-                governor_of_that_state = Governor.objects.get(state_id=states.pk)
-                name = GovernorSerializer(governor_of_that_state).data.get('name', None)
-                deputy = DeputyGovernorSerializer(DeputyGovernor.objects.get(state_id=states.pk)).data.get('name',
-                                                                                                           None)
-                return Response({"Governor": name, "Deputy Governor": deputy})
+        name_of_state = request.data.get('name_of_state').capitalize()
+        state = NigerianState.objects.get(name_of_state=name_of_state)
+        name = GovernorSerializer(Governor.objects.get(state_id=state.pk)).data.get('name', None)
+        deputy = DeputyGovernorSerializer(DeputyGovernor.objects.get(state_id=state.pk)).data.get('name', None)
+        return Response({"Governor": name, "Deputy Governor": deputy})
